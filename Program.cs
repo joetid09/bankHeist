bf2be4178d3998fda5d5b3dfc924a1ea0a4b837e
joseph tidwell <joseph.tidwell09@gmail.com>
2020-11-20 10:34:25 -0600

    displaying Plan your heist, step one complete

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankHeist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plan your Heist!");
+        }
+    }
+}
---
+++ TeamMate.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BankHeist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
